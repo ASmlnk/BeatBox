d5d1a9aa959c9bd17a103872dbcb20dff6dc8604
user <sdelanss@gmail.com>
2023-03-02 19:44:37 +0300

    Initial commit

---
+++ app/src/main/java/combignerdranch/android/beatbox/MainActivity.kt
@@ -0,0 +1,20 @@
+package combignerdranch.android.beatbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import combignerdranch.android.beatbox.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
+    }
+}
